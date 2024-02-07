d63640b617d84cac17787307bd3e912be4084c35
janghyunjun <buzz03312@naver.com>
2024-02-07 16:31:19 +0900

    노래삽입/시작

--- game.js
+++ game.js
@@ -9,9 +9,29 @@ $(document).ready(function() {
 
     var currentCharacterIndex = 0;
 
-    setInterval(function() {
-        currentCharacterIndex = (currentCharacterIndex + 1) % characterImages.length;
-        character.attr('src', characterImages[currentCharacterIndex]);
-    }, 100); 
+    $('#image').css('animation-play-state', 'paused');
     
+
+
+    //bgm 시작
+    var bgm = new Audio('sound_pack/Beautiful_Days.wav');
+    bgm.pause();
+
+    //key 눌렀을떄 게임시작
+    document.addEventListener("keydown", function(event){
+        startGame();
+    });
+    
+    function startGame(){
+        bgm.play();
+        setInterval(function() {
+            currentCharacterIndex = (currentCharacterIndex + 1) % characterImages.length;
+            character.attr('src', characterImages[currentCharacterIndex]);
+        }, 100); 
+        $('#image').css('animation-play-state', 'running');
+    };
 });
+
+
+
+
