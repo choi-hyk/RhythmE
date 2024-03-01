4db7cbed9cd6309b4685b62777f463b7607ef421
CHOI HYUK <8167chhk@naver.com>
2024-03-01 18:01:05 +0900

    hhh

--- characters.js
+++ characters.js
@@ -13,6 +13,11 @@ const shadowImages = [
     "game_img/shadow5.png"
 ];
 
+var hatColor = 'pink';
+
+var clothesColor = 'pink';
+
+
 $(document).ready(function() {    
     var hatIndex = 1;
 
@@ -23,15 +28,23 @@ $(document).ready(function() {
         switch (hatIndex) {
             case 1:
                 $('#hat img').attr("src", "clothes/pink_hat1.png");
+                hat.attr("src", "clothes/pink_hat" + currentClothesIndex + ".png");
+                hatColor = 'pink';
                 break;
             case 2:
                 $('#hat img').attr("src", "clothes/green_hat1.png");
+                hat.attr("src", "clothes/green_hat" + currentClothesIndex + ".png");
+                hatColor = 'green';
                 break;
             case 3:
                 $('#hat img').attr("src", "clothes/yellow_hat1.png");
+                hat.attr("src", "clothes/yellow_hat" + currentClothesIndex + ".png");
+                hatColor = 'yellow';
                 break;
             case 4:
                 $('#hat img').attr("src", "clothes/blue_hat1.png");
+                hat.attr("src", "clothes/blue_hat" + currentClothesIndex + ".png");
+                hatColor = 'blue';
                 break;
             default:
                 break;
@@ -42,15 +55,23 @@ $(document).ready(function() {
         switch (clothesIndex) {
             case 1:
                 $('#clothes img').attr("src", "clothes/pink1.png");
+                clothes.attr("src", "clothes/pink" + currentClothesIndex + ".png");
+                clothesColor = 'pink';
                 break;
             case 2:
                 $('#clothes img').attr("src", "clothes/green1.png");
+                clothes.attr("src", "clothes/green" + currentClothesIndex + ".png");
+                clothesColor = 'green';
                 break;
             case 3:
                 $('#clothes img').attr("src", "clothes/yellow1.png");
+                clothes.attr("src", "clothes/yellow" + currentClothesIndex + ".png");
+                clothesColor = 'yellow';
                 break;
             case 4:
                 $('#clothes img').attr("src", "clothes/blue1.png");
+                clothes.attr("src", "clothes/blue" + currentClothesIndex + ".png");
+                clothesColor = 'blue';
                 break;
             default:
                 break;
@@ -69,7 +90,7 @@ $(document).ready(function() {
             hatIndex -= 1;
         }
         setHatImages(hatIndex);
-        console.log("Current hat: " + hatIndex);
+        console.log("Current hat: " + hatColor);
         
     });
 
@@ -85,7 +106,7 @@ $(document).ready(function() {
             hatIndex += 1;
         }
         setHatImages(hatIndex);
-        console.log("Current hat: " + hatIndex);
+        console.log("Current hat: " + hatColor);
     });
 
     ///////////////////////////////
@@ -102,7 +123,7 @@ $(document).ready(function() {
             clothesIndex -= 1;
         }
         setClothesImages(clothesIndex);
-        console.log("Current Difficulty: " + clothesIndex);
+        console.log("Current Difficulty: " + clothesColor);
         
     });
 
@@ -118,7 +139,7 @@ $(document).ready(function() {
             clothesIndex += 1;
         }
         setClothesImages(clothesIndex);
-        console.log("Current Difficulty: " + clothesIndex);
+        console.log("Current Difficulty: " + clothesColor);
     });
     
 /////////////////////////////////////////////////////
@@ -137,12 +158,18 @@ $('#back img').hover(function(){
     });
 
 });
+/////////////////////////////////////////////////////////
 
     var jumping = false;
     var character = $('.rhythme');
     var shadow = $('.shadow');
+    var hat = $('.wearing_hat');
+    var clothes = $('.wearing_clothes');
+
     var currentCharacterIndex = 0;
+    var currentClothesIndex = 2;
     var currentShadowIndex = 1;
+
     // 게임 시작 및 점프 이벤트 핸들러
     document.addEventListener("keydown", function(event) {
         
@@ -155,10 +182,34 @@ $('#back img').hover(function(){
     var imageInterval;
 
     function JumpAction() {
+        
         jumping = true;
+
         clearInterval(imageInterval);
         character.attr('src', characterImages[0]);
         shadow.attr('src', shadowImages[4]);
+        hat.attr('src',"clothes/" + hatColor + "_hat2.png");
+        clothes.attr('src',"clothes/" + clothesColor + "2.png");
+
+        hat.animate({ top: '39%' }, 190, 'linear')
+        .animate({ top: '43%' }, 150, 'linear', function() {
+            hat.attr('src',"clothes/" + hatColor + "_hat2.png");
+            jumping = false;
+            imageInterval = setInterval(function() {
+                hat.attr('src',"clothes/" + hatColor + "_hat" + currentClothesIndex + ".png");
+            }, 100);
+        });
+
+        clothes.animate({ top: '39%' }, 190, 'linear')
+        .animate({ top: '43%' }, 150, 'linear', function() {
+            clothes.attr('src',"clothes/" + clothesColor + "2.png");
+            jumping = false;
+            imageInterval = setInterval(function() {
+                currentClothesIndex = 1 + (currentClothesIndex + 1) % 4;
+                clothes.attr('src',"clothes/" + clothesColor + currentClothesIndex + ".png");                    
+            }, 100);
+        });
+
         character.animate({ top: '39%' }, 190, 'linear')
             .animate({ top: '43%' }, 150, 'linear', function() {
                 character.attr('src', characterImages[0]);
@@ -168,16 +219,9 @@ $('#back img').hover(function(){
                     currentCharacterIndex = (currentCharacterIndex + 1) % characterImages.length;
                     currentShadowIndex = (currentShadowIndex + 1) % (shadowImages.length-1);
                     character.attr('src', characterImages[currentCharacterIndex]);
-                
                     shadow.attr('src',shadowImages[currentShadowIndex]);
-                    
                 }, 100);
             });
     }
-
-    setTimeout(() => {
-        JumpAction();
-    },1000);
-
 
 });
