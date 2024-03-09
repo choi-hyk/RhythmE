7bbd5903ff63b44e312eb115a4dbab14259c093c
janghyunjun <buzz03312@naver.com>
2024-03-09 16:09:08 +0900

    튜토리얼

--- tutorial.js
+++ tutorial.js
@@ -102,7 +102,8 @@ $(document).ready(function() {
     var instruction = $('.instruction');
 
     var instructionInterval;  
-
+    var moveInterval;
+    var idx=0;
     function init() {
 
         start_check = false;
@@ -115,7 +116,6 @@ $(document).ready(function() {
 
 
         clearInterval(imageInterval);
-        clearInterval(gamingInterval);
 
         character.attr('src', 'characters_img/rhythme0.png');
 
@@ -143,7 +143,7 @@ $(document).ready(function() {
             instruction.attr('src', instructionImages[index]);
             index = (index + 1) % instructionImages.length;
         }, 500);     
-
+        console.log(1234);
         
     }
 
@@ -152,16 +152,35 @@ $(document).ready(function() {
     function hideInstructionImage() {
         instruction.hide();
     }
-
+    
+    
     document.addEventListener("keydown", function(event) {
 
         if (!start_check && (event.key === 's' || event.key === 'S' || event.key === 'k' || event.key === 'K')) {
 
             start_check = true;
             
-            moveBlock();
-
-            hideInstructionImage();
+            
+            
+            
+            
+            
+            if(idx<=1){
+                moveBlock(idx);
+                hideInstructionImage();
+                init();
+            }
+            if(idx==2){
+                moveBlock(idx);
+                hideInstructionImage();
+            }
+            idx++;
+            
+            
+            
+            
+                
+            
 
         $('#image').css('animation-play-state', 'running');
 
@@ -177,7 +196,6 @@ $(document).ready(function() {
 
     var jumping = false;
     var imageInterval;
-    var gamingInterval;
     var sadCheck = false;
 
     function JumpAction() {
@@ -190,41 +208,51 @@ $(document).ready(function() {
 
         shadow.attr('src', shadowImages[4]);
 
-        hatImg.attr('src',"clothes_img/" + hatColor + "_hat2.png");
-        
-        clothesImg.attr('src',"clothes_img/" + clothesColor + "_clothes2.png");
-
-        hatImg.animate({ top: '65%' }, 190, 'linear')
-        .animate({ top: '75%' }, 150, 'linear', function() {
+        if(hatParameter){
             hatImg.attr('src',"clothes_img/" + hatColor + "_hat2.png");
-            jumping = false;
-            
-        });
-
-        clothesImg.animate({ top: '65%' }, 190, 'linear')
-        .animate({ top: '75%' }, 150, 'linear', function() {
+            hatImg.animate({ top: '65%' }, 190, 'linear')
+            .animate({ top: '75%' }, 150, 'linear', function() {
+                hatImg.attr('src',"clothes_img/" + hatColor + "_hat2.png");
+                jumping = false;
+                
+            });
+            console.log(1);
+        }
+        
+        if(clothesParameter){
             clothesImg.attr('src',"clothes_img/" + clothesColor + "_clothes2.png");
-            jumping = false;
-           
-        });
-
+            clothesImg.animate({ top: '65%' }, 190, 'linear')
+            .animate({ top: '75%' }, 150, 'linear', function() {
+                clothesImg.attr('src',"clothes_img/" + clothesColor + "_clothes2.png");
+                jumping = false;
+            
+            }); 
+        }
         character.animate({ top: '65%' }, 190, 'linear')
             .animate({ top: '75%' }, 150, 'linear', function() {
                 character.attr('src', characterImages[3]);
                 shadow.attr('src',shadowImages[0]);
 
-                hatImg.attr('src',"clothes_img/" + hatColor + "_hat5.png");
-                clothesImg.attr('src',"clothes_img/" + clothesColor + "_clothes5.png");
+                if(hatParameter){
+                    hatImg.attr('src',"clothes_img/" + hatColor + "_hat5.png");
+                }
+                if(clothesParameter){
+                    clothesImg.attr('src',"clothes_img/" + clothesColor + "_clothes5.png");
+                }
                
                          
                 jumping = false;
                 imageInterval = setInterval(function() {                 
                     currentCharacterIndex = (currentCharacterIndex + 1) % characterImages.length;
                     currentShadowIndex = (currentShadowIndex + 1) % (shadowImages.length-1);
-                    clothesImg.attr('src',"clothes_img/" + clothesColor + "_clothes" + (currentCharacterIndex + 2) + ".png");  
-                    hatImg.attr('src',"clothes_img/" + hatColor + "_hat" + (currentCharacterIndex + 2) + ".png");
-                    hatImg.css('top','75%');
-                    clothesImg.css('top','75%');
+                    if(clothesParameter){
+                        clothesImg.attr('src',"clothes_img/" + clothesColor + "_clothes" +(currentCharacterIndex + 2) + ".png");  
+                        clothesImg.css('top','75%');
+                    }
+                    if(hatParameter){
+                        hatImg.attr('src',"clothes_img/" + hatColor + "_hat" + (currentCharacterIndex + 2) + ".png");
+                        hatImg.css('top','75%');
+                    }
                     if(!sadCheck){
                         character.attr('src', characterImages[currentCharacterIndex]);
                     }else{
@@ -247,181 +275,174 @@ $(document).ready(function() {
 
     }
 
-    const totorialBlocks = [
+    const tutorialBlocks = [
         new Block(1000, 'yellow', 13, 5),
         new Block(2000, 'blue', 13, 5),
-        new Block(3000, 'green', 13, 5)];
+        new Block(3000, 'green', 13, 5)
+    ];
 
     var currentTime;
     var blocks = [];
 
-    function moveBlock() {
+    async function moveBlock(i) {
 
-      blocks = totorialBlocks;
-
-        blocks.forEach(block => {
-            setTimeout(() => {
-                createBlock(block);
-
-                var cal_speed = 0;
-                //1초에 블록이 움직이는 거리 계산
-                if(block.speed < 10){
-                    cal_speed = (1000/(block.speed-0.5))*block.movePx;
-                }else{
-                    cal_speed = (1000/(block.speed-0.14)) *block.movePx;
-                }
-                // pointTime = (window.innerWidth * 0.7 / cal_speed) * 1000
-                
-
-                //블록 생성되었을때 이벤트 리스너 생성
-                setTimeout(() => {
+        blocks = tutorialBlocks;
+        var correct = null;  
+        
+            
+        const block_img = document.createElement('img');
+        block_img.classList.add('block');
+        block_img.src = 'items_img/'+blocks[i].color+'_block.gif';
+        
+        block_img.style.left = window.innerWidth+'px'
+        document.body.appendChild(block_img);
+        
+        moveInterval = setInterval(async function(){
+            
+                const currentLeft = parseFloat(block_img.style.left);
+                if(currentLeft <= window.innerWidth*0.28 || currentLeft <= -30) {
+                    clearInterval(moveInterval);
                     
-                    enableKeyPress(block.color);
-                    correct_key = false;
-                }, ((window.innerWidth * 0.7 / cal_speed) *1000));
+                    correct = await enableKeyPress(blocks[i].color);
+                    
+                    console.log(blocks[i].color);
+                    block_img.remove();
+                    return;
 
-                //블록 지나갔을때 이벤트 리스너 제거
-                setTimeout(() => {
-                    document.removeEventListener("keydown", handleKeyPress);
-                    document.removeEventListener("keydown", handleSameKeyPress);
-                  
-                }, ((window.innerWidth * 0.7 / cal_speed) *1000)+300)
-            },block.time);
-        });
+                    
+                } else {
+                    block_img.style.left = (currentLeft - blocks[i].movePx)+'px'
+            } 
+        }, blocks[i].speed);
+        
+        
+        return correct;
+        
     }
 
     var block_color
     
-    //알맞은 키를 눌렀는지 여부
-    var correct_key = false;
+    var correct_key = false;    //알맞은 키를 눌렀는지 여부
     var green_sCheck = false; //green block일때 s를 눌렀는지 확인
     var green_kCheck = false; //green block일때 k를 눌렀는지 확인
 
     // 블록이 일정 위치에 도달했을 때 키 입력을 받도록 설정하는 함수
-    function enableKeyPress(blockColor) {
-        var d = new Date();
-                currentTime = d.getMilliseconds();
-        block_color = blockColor;
-
+    function enableKeyPress(blockColor) { //return값이 true면 다음 block으로 넘어감, false면 다시 그 block 생성
+        
+        
         clearInterval(imageInterval);
+        // clearInterval(moveInterval);
         
-        document.addEventListener("keydown", handleKeyPress);
-        document.addEventListener("keydown", handleSameKeyPress);
-        
-        
-    }
-
-    function createBlock(block){
-        const block_img = document.createElement('img');
-        block_img.classList.add('block');
-        block_img.src = 'items_img/'+block.color+'_block.gif';
-        block_img.style.left = window.innerWidth+'px'
-        document.body.appendChild(block_img);
-
-        const moveInterval = setInterval(() => {
-                const currentLeft = parseFloat(block_img.style.left);
-                if(currentLeft <= -30) {
-                    clearInterval(moveInterval);
-                    block_img.remove();
-                } else {
-                    block_img.style.left = (currentLeft - block.movePx)+'px'
-            } 
-        }, block.speed);
-    }
-
-    function handleKeyPress(event) {
-        var date = new Date();
-        var timegap = date.getMilliseconds() - currentTime;
-        // green 블록이면 's'와 'k'를 동시에 눌러야 함
-        if (block_color === 'green' && (event.key === 's' || event.key === 'S')) {
-            if(green_kCheck == false){ //green block일 때 k를 아직 누르지 않았다면
-                green_sCheck = true;   //s 눌렀다고 표시
-            } else{     //k를 이미 누르고 s를 누른 상황
-                if((timegap < 100 && timegap >= 50) || (timegap > 200 && timegap <= 250)){
-                    showAchievement(1);
-                }
-                else if(timegap <50 || timegap > 250){
-                    showAchievement(0);
-                }
-               else{
-                showAchievement(2);
-               }
-                green_kCheck = false;
-                correct_key = true;
+        return new Promise(function(resolve){
+            switch(blockColor){
+                case "blue":
+                    document.addEventListener("keydown", handleKPress);
+                    
+                    break;
+                case "yellow":
+                    document.addEventListener("keydown", handleSPress);
+                    
+                    break;
+                case "green":
+                    document.addEventListener("keydown", handleKeyPress);
+                    document.addEventListener("keydown", handleSameKeyPress);
+                    break;        
             }
-            document.removeEventListener("keydown", handleKeyPress); // 이벤트 리스너 삭제
-        }
-        // blue 블록이면 'k'를 눌러야 함
-        else if (block_color === 'blue' && (event.key === 'k' || event.key === 'K')) {
-            correct_key = true;
-            if((timegap < 100 && timegap >= 50) || (timegap > 200 && timegap <= 250)){
-                showAchievement(1);
-            }
-            else if(timegap <50 || timegap > 250){
-                showAchievement(0);
-            }
-           else{
-            showAchievement(2);
-           }
-            document.removeEventListener("keydown", handleSameKeyPress);
-            document.removeEventListener("keydown", handleKeyPress); // 이벤트 리스너 삭제
-        }
-        // yellow 블록이면 's'를 눌러야 함
-        else if (block_color === 'yellow' && (event.key === 's' || event.key === 'S')) {
-            correct_key = true;
-            if((timegap < 100 && timegap >= 50) || (timegap > 200 && timegap <= 250)){
-                showAchievement(1);
-            }
-            else if(timegap <50 || timegap > 250){
-                showAchievement(0);
-            }
-           else{
-            showAchievement(2);
-           }
-            document.removeEventListener("keydown", handleSameKeyPress);
-            document.removeEventListener("keydown", handleKeyPress); // 이벤트 리스너 삭제
-        }
-        
-       
-    }
-
-    function handleSameKeyPress(event){
-        var date = new Date();
-        var timegap = date.getMilliseconds() - currentTime;
-        if (block_color === 'blue' && (event.key === 's' || event.key === 'S')) {
-            correct_key = true;
-            showAchievement(3);
-            console.log('점수감소');
-            document.removeEventListener("keydown", handleSameKeyPress); // 이벤트 리스너 삭제
-            document.removeEventListener("keydown", handleKeyPress);
-        }
-        else if (block_color === 'yellow' && (event.key === 'k' || event.key === 'K')) {
-            correct_key = true;
-            showAchievement(3);
-            console.log('점수감소');
-            document.removeEventListener("keydown", handleSameKeyPress); // 이벤트 리스너 삭제
-            document.removeEventListener("keydown", handleKeyPress);
-        }
-        if (block_color === 'green' && (event.key === 'k' || event.key === 'K')) {
-            if(green_sCheck == true){ //s를 누르고 k를 누른 상황
-                if((timegap < 100 && timegap >= 50) || (timegap > 200 && timegap <= 250)){
-                    showAchievement(1);
-                }
-                else if(timegap <50 || timegap > 250){
-                    showAchievement(0);
-                }
-               else{
-                showAchievement(2);
-               }
-                green_sCheck=false;
-                correct_key = true;
-            } else {
-                green_kCheck = true;
-            }
+            console.log("알1");
             
-            document.removeEventListener("keydown", handleSameKeyPress); // 이벤트 리스너 삭제
-        }
+
+            function handleKeyPress(event) {
+        
+        
+                // green 블록이면 's'와 'k'를 동시에 눌러야 함
+                if (event.key === 's' || event.key === 'S') {
+                    if(green_kCheck == false){ //green block일 때 k를 아직 누르지 않았다면
+                        green_sCheck = true;   //s 눌렀다고 표시
+                    } else{     //k를 이미 누르고 s를 누른 상황
+                        
+                        showAchievement(2);
+                        
+                        green_kCheck = false;
+                        correct_key = true;
+                        console.log("성공");
+                        ///////성공 페이지 추가///////
+                        resolve(true);
+                    }
+                    document.removeEventListener("keydown", handleKeyPress); // 이벤트 리스너 삭제
+                }
+                
+            }
+        
+            function handleSameKeyPress(event){
+                
+                if (event.key === 'k' || event.key === 'K') {
+                    if(green_sCheck == true){ //s를 누르고 k를 누른 상황
+                        
+                        showAchievement(2);
+                       
+                        green_sCheck=false;
+                        correct_key = true;
+                        console.log("성공");
+                        ///////성공 페이지 추가///////
+                        resolve(true);
+                        
+                    } else {
+                        green_kCheck = true;
+                    }
+                    
+                    document.removeEventListener("keydown", handleSameKeyPress); // 이벤트 리스너 삭제
+                }
+            }
+        
+            function handleKPress(event){
+                
+                // blue 블록이면 'k'를 눌러야 함
+                if (event.key === 'k' || event.key === 'K') {
+                    correct_key = true;
+                    
+                    showAchievement(2);
+                    
+                    document.removeEventListener("keydown", handleKPress); // 이벤트 리스너 삭제
+                    resolve(true);
+                }
+                else if (event.key === 's' || event.key === 'S') {
+                    correct_key = false;
+                    showAchievement(3);
+                    
+                    document.removeEventListener("keydown", handleKPress); // 이벤트 리스너 삭제
+                    resolve(false);
+                }
+            }
+        
+            function handleSPress(event){
+                
+                if (event.key === 's' || event.key === 'S') {
+                    correct_key = true;
+                   
+                    showAchievement(2);
+                    
+                    
+                    document.removeEventListener("keydown", handleSPress); // 이벤트 리스너 삭제
+                    
+                    resolve(true);
+                    
+                }
+                else if (event.key === 'k' || event.key === 'K') {
+                    correct_key = false;
+                    showAchievement(3);
+                    
+                    document.removeEventListener("keydown", handleSPress); // 이벤트 리스너 삭제
+                    
+                    resolve(false);
+                }
+            }
+        });
+        
+        
+        
     }
+
+   
 
     const scoreImages = ['cool_text.png', 'great_text.png', 'perfect_text.png','miss_text.png'];
 
